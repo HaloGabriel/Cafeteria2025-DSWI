f5c3360c4ad8c75f3316f029e612fc9796889249
HaloGabriel <carlosgbm2000@gmail.com>
2025-12-08 19:48:27 -0500

    DAO Categoría
    - Interface ICategoriaDAO Creada
    - Clase CategoriaDAOImpl Creada
    - Program.cs Modificado

---
+++ Cafeteria2025_API_REST/DAO/ICategoriaDAO.cs
@@ -0,0 +1,13 @@
+﻿using Cafeteria2025_API_REST.Models;
+
+namespace Cafeteria2025_API_REST.DAO
+{
+    public interface ICategoriaDAO
+    {
+        Task<IEnumerable<Categoria>> Listar();
+        Task<IEnumerable<Categoria>> ListarDescripcionAsc();
+        Task<Categoria> Buscar(int id);
+        void Insertar(Categoria reg);
+        void Actualizar(Categoria reg);
+    }
+}
--- Cafeteria2025_API_REST/Program.cs
+++ Cafeteria2025_API_REST/Program.cs
@@ -1,6 +1,10 @@
+using Cafeteria2025_API_REST.DAO;
+using Cafeteria2025_API_REST.DAO.Impl;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ICategoriaDAO, CategoriaDAOImpl>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
