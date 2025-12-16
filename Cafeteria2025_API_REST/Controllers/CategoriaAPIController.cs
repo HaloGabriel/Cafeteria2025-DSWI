ae469102fb550f855545c31abc5c41150e7d9d8f
HaloGabriel <carlosgbm2000@gmail.com>
2025-12-16 11:27:02 -0500

    Endpoints de categoría corregidos

--- Cafeteria2025_API_REST/Controllers/CategoriaAPIController.cs
+++ Cafeteria2025_API_REST/Controllers/CategoriaAPIController.cs
@@ -17,7 +17,7 @@ namespace Cafeteria2025_API_REST.Controllers
         // ===============================
         // LISTAR CATEGORIAS
         // ===============================
-        [HttpGet("Lista")] public async Task<ActionResult> Lista()
+        [HttpGet("Lista")] public async Task<ActionResult<List<Categoria>>> Lista()
         {
             var lista = await categoriaDAO.Listar();
             return Ok(lista);
@@ -26,7 +26,7 @@ namespace Cafeteria2025_API_REST.Controllers
         // ===============================
         // LISTAR CATEGORIAS POR DESCRIPCION
         // ===============================
-        [HttpGet("Lista/Sort/Descripcion")] public async Task<ActionResult> ListaDescripcionAsc()
+        [HttpGet("Lista/Sort/Descripcion")] public async Task<ActionResult<CategoriaSelectList>> ListaDescripcionAsc()
         {
             var lista = await categoriaDAO.ListarDescripcionAsc();
             return Ok(lista);
@@ -35,27 +35,37 @@ namespace Cafeteria2025_API_REST.Controllers
         // ===============================
         // BUSCAR POR ID
         // ===============================
-        [HttpGet("{id}")] public async Task<ActionResult> BuscarPorId(int id = 0)
+        [HttpGet("{id}")] public async Task<ActionResult<Categoria>> BuscarPorId(int id = 0)
         {
             var categoria = await categoriaDAO.Buscar(id);
+
+            if (categoria == null)
+                return NotFound("¡Categoría no encontrada!");
+
             return Ok(categoria);
         }
 
         // ===============================
         // REGISTRAR CATEGORIA
         // ===============================
-        [HttpPost] public async Task<ActionResult> Registrar(Categoria reg)
+        [HttpPost] public async Task<ActionResult> Registrar(CategoriaCreate reg)
         {
-            await Task.Run(() => categoriaDAO.Insertar(reg));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await categoriaDAO.Insertar(reg);
             return Ok("¡Categoría registrada!");
         }
 
         // ===============================
         // ACTUALIZAR CATEGORIA
         // ===============================
-        [HttpPut] public async Task<ActionResult> Actualizar(Categoria reg)
+        [HttpPut] public async Task<ActionResult> Actualizar(CategoriaUpdate reg)
         {
-            await Task.Run(() => categoriaDAO.Actualizar(reg));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await categoriaDAO.Actualizar(reg);
             return Ok("¡Categoría actualizada!");
         }
     }
