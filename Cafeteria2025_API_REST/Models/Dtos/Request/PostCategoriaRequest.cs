dba04fc61158f8647866a466ff1d9922704c330c
HaloGabriel <carlosgbm2000@gmail.com>
2025-12-16 11:00:24 -0500

    DTOs obsoletos eliminados

--- Cafeteria2025_API_REST/Models/Dtos/Request/PostCategoriaRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Cafeteria2025_API_REST.Models.Dtos.Request
-{
-    public class PostCategoriaRequest
-    {
-        public string name { get; set; } = string.Empty;
-    }
-}
--- Cafeteria2025_API_REST/Models/Dtos/Request/PutCategoriaRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cafeteria2025_API_REST.Models.Dtos.Request
-{
-    public class PutCategoriaRequest
-    {
-        public int id { get; set; }
-        public string name { get; set; } = string.Empty;
-        public bool enabled { get; set; }
-    }
-}
